e39192247ad90050baec664f20ec35ac2ff337ee
Max Häggman <max.haggman@edu.cmeducations.se>
2023-08-18 11:49:24 +0200

    add languages data and component to app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Languages from './components/languages/languages';
 
 function App() {
   return (
     <div className="App">
-      
+      <Languages />
     </div>
   );
 }
---
+++ src/languages.js
@@ -0,0 +1,15 @@
+export const languages = [
+    { id: 1, name: 'Svenska', articles: "2 568 000", articleText: 'artiklar' },
+    { id: 2, name: 'English', articles: "6 691 000", articleText: 'articles' },
+    { id: 3, name: 'Latina', articles: "138 000", articleText: 'commentationes'},
+    { id: 4, name: '日本語', articles: "1 382 000", articleText: '記事' },
+    { id: 5, name: 'Español', articles: "1 881 000", articleText: 'artículos' },
+    { id: 6, name: 'Русский', articles: "1 930 000", articleText: 'статей' },
+    { id: 7, name: 'Deutsch', articles: "2 822 000", articleText: 'Artikel' },
+    { id: 8, name: 'Français', articles: "2 540 000", articleText: 'articles' },
+    { id: 9, name: 'Italiano', articles: "1 820 000", articleText: 'voci' },
+    { id: 10, name: '中文', articles: "1 369 000", articleText: '条目 / 條目' }
+];
+  
+  
+  
