b55fbfd35dbc9d92bca1431f5e54b1792771d8be
Max Häggman <max.haggman@edu.cmeducations.se>
2023-08-18 14:23:38 +0200

    add logo imports

--- src/components/languages/languages.jsx
+++ src/components/languages/languages.jsx
@@ -1,6 +1,8 @@
 import { languages } from "../../languages"
 import Language from "../language/langauge"
 import './index.css'
+import logo from '../../images/Wikipedia-logo.png'
+import { Image } from "../img/image"
 
 export default function Languages() {
     let top = 0;
