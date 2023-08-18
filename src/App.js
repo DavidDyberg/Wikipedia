0cb21762b1cefe3efa7e160948ed299c90493c8d
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-08-18 17:58:52 +0200

    made footer into a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Languages from './components/languages/languages';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
       <Languages />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+
+export default function Footer () {
+    return(
+        <>
+         <footer>
+            <hr></hr>
+            <small>Copyright &#169; Created by Max Edu and David: 2023</small>
+        </footer>
+        </>
+    )
+}
