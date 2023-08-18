e1a40c756492f3a6bd42a12314b9612facd23fec
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-08-18 14:55:23 +0200

    made header into a component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Languages from './components/languages/languages';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Languages />
     </div>
   );
---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+
+export default function Header() {
+    return(
+        <>
+        <header>
+        <h1>Wikipedia</h1>
+        <p className='subheading'>Den fria encyklopedin</p>
+      </header>
+        </>
+    )
+}
