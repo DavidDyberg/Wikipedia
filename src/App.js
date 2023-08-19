729c758b3cadcaee59bdde1613c9a93b5822e1d0
Edu Garma Andersen <edugarma@CyberThanos-2.local>
2023-08-19 19:39:17 +0200

    searchbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Languages from './components/languages/languages';
+import SearchBar from "./components/searchbar/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <Languages />
+      <SearchBar/>
     </div>
   );
 }
