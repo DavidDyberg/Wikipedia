0509f7e50675be452d127eae8e38157e8d9fef41
Edu Garma Andersen <edugarma@CyberThanos-2.local>
2023-08-21 13:50:03 +0200

    added css

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -3,11 +3,12 @@ import "../searchbar/index.css";
 
 const SearchBar = () => {
   return (
-    <div className="cont">
+    <div className="cont" >
       <form className="form">
         <input type="text" className="form-input" name="search" />
       </form>
     </div>
   );
 };
+
 export default SearchBar;
