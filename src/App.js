5ee6243a3d526b54aacb330a71b19e2547c45b6d
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-08-25 11:10:33 +0200

    add footer-top

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Languages from './components/languages/languages';
 import Header from './components/header';
+import FooterTop from './components/footerTop/footerTop';
 import Footer from './components/footer/footer';
 import SearchBar from './components/searchbar/SearchBar'
 
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Languages />
       <SearchBar/>
+      <FooterTop />
       <Footer />
     </div>
   );
